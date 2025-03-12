f67379604d58496f33b55a53af0a0818b7474061
VladislavBugaenko <Vladislav.Bugaenko@evraz.com>
2025-03-12 17:47:08 +0500

    Начал реализацию стриминга на SignalR

---
+++ Domain.EchoPlay/Interfaces/IStreamingClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.EchoPlay.Interfaces;
+
+public interface IStreamingClient
+{
+    Task SendMessageAsync(byte[] data);
+    
+    Task StartStreamingAsync();
+    Task StopStreamingAsync();
+    
+}
