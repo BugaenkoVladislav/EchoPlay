bcff9cb040c8c07eb0513864723265b9ef3fe3a6
BugaenkoVladislav <bugaenkovlad201@gmail.com>
2025-04-27 12:04:00 -0700

    Добавил мелочей для организации

---
+++ App.EchoPlay/Services/RoomService.cs
@@ -0,0 +1,9 @@
+using Infrastructure.EchoPlay;
+using Infrastructure.EchoPlay.Hubs;
+
+namespace App.EchoPlay.Services;
+
+public class RoomService(RoomHub chatHub,UnitOfWork uow)
+{
+    
+}
---
+++ App.EchoPlay/Services/StreamingService.cs
@@ -0,0 +1,6 @@
+namespace App.EchoPlay.Services;
+//todo сделать фабрику на выбор стриминга
+public class StreamingService
+{
+    
+}
