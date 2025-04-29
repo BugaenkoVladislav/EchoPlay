6297594582c4646d8c4050c05fabe1996e4b2941
BugaenkoVladislav <bugaenkovlad201@gmail.com>
2025-04-29 11:47:56 -0700

    Дополнил интерфейс

--- Infrastructure.EchoPlay/Hubs/RoomHub.cs
+++ Infrastructure.EchoPlay/Hubs/RoomHub.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Infrastructure.EchoPlay.Hubs;
 
-public class RoomHub : Hub
+public class RoomHub : Hub, IRoom
 {
     // Метод для присоединения пользователя к комнате
     public async Task JoinRoom(string roomName)
