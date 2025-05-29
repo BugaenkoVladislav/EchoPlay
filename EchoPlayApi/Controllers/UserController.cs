f29855337ea1de9f7b873542b76ddff09e48c0a2
BugaenkoVladislav <bugaenkovlad201@gmail.com>
2025-05-29 21:34:34 +0500

    Еще небольшая коррекция

---
+++ EchoPlayApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+using App.EchoPlay.Dtos;
+using App.EchoPlay.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EchoPlayApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(UserService userService) : ControllerBase
+    {
+        private readonly UserService _userService = userService;
+        [HttpPost("add-photo")]
+        public async Task AddUserPhotoAsync([FromBody] UsernamePhotoDto dto)
+        {
+            
+           await _userService.AddUserPhoto(dto.UserId, dto.PhotoPath); 
+        }
+        [HttpPost("get-photo")]
+        public async Task<string> GetPhotoAsync([FromQuery]string userId)
+        {
+            userId = userId.Trim('"', ' ', '\t', '\n', '\r');
+            var guid = Guid.Parse(userId);
+            var photo = await _userService.GetUserPhoto(guid);
+            return photo;
+        }
+
+        [HttpGet("get-user-id/{username}")]
+        public async Task<Guid> GetUserId(string username)
+        {
+            return await _userService.GetUserId(username);
+        }
+    }
+}
